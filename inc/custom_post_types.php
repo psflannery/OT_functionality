c29bdb80a554340d2ff6b5be5d578d3e75b175f1
psflannery <psflannery@gmail.com>
2016-01-16 23:20:28 +0000

    remove take-over post type

--- inc/custom_post_types.php
+++ inc/custom_post_types.php
@@ -121,53 +121,6 @@ function register_cpt_article() {
 	register_post_type( 'article', $args );
 }
 
-/**
- * Add the Take Over Post Type 
- *
- * TO BE DEPRECIATED
- */
-add_action( 'init', 'register_cpt_take_over' );
-function register_cpt_take_over() {
-
-	$labels = array( 
-		'name' => _x( 'Take Overs', 'take over' ),
-		'singular_name' => _x( 'Take Over', 'take over' ),
-		'add_new' => _x( 'Add New', 'take over' ),
-		'add_new_item' => _x( 'Add New Take Over', 'take over' ),
-		'edit_item' => _x( 'Edit Take Over', 'take over' ),
-		'new_item' => _x( 'New Take Over', 'take over' ),
-		'view_item' => _x( 'View Take Over', 'take over' ),
-		'search_items' => _x( 'Search Take Overs', 'take over' ),
-		'not_found' => _x( 'No take overs found', 'take over' ),
-		'not_found_in_trash' => _x( 'No take overs found in Trash', 'take over' ),
-		'parent_item_colon' => _x( 'Parent Take Over:', 'take over' ),
-		'menu_name' => _x( 'Take Overs', 'take over' ),
-	);
-
-	$args = array( 
-		'labels' => $labels,
-		'hierarchical' => false,
-		'description' => 'The take over post type, an archive of the Opening Times take overs',
-		'supports' => array( 'title', 'editor', 'thumbnail', 'revisions' ),
-		'taxonomies' => array( 'category', 'post_tag', 'artists' ),
-		'public' => true,
-		'show_ui' => true,
-		'show_in_menu' => true,
-		'menu_position' => 5,
-		'menu_icon' => 'dashicons-admin-site',
-		'show_in_nav_menus' => true,
-		'publicly_queryable' => true,
-		'exclude_from_search' => false,
-		'has_archive' => true,
-		'query_var' => true,
-		'can_export' => true,
-		'rewrite' => true,
-		'capability_type' => 'post'
-	);
-
-	register_post_type( 'take-overs', $args );
-}
-
 /* Add the Projects Post Type */
 add_action( 'init', 'register_cpt_projects' );
 function register_cpt_projects() {
